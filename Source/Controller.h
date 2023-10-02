2019ad4327ee11844e265b820050a6c256bfed38
DreamHollow <404errorman@gmail.com>
2023-10-02 00:11:00 -0400

    Controller.h: Erased invalid data

--- Source/Controller.h
+++ Source/Controller.h
@@ -1,15 +1,15 @@
-#ifndef CONTROLLER_H_INCLUDED
-#define CONTROLLER_H_INCLUDED 1
+//#ifndef CONTROLLER_H_INCLUDED
+//#define CONTROLLER_H_INCLUDED 1
 
-#include "Maths/glm.h"
-#include <SFML/Graphics.hpp>
+//#include "Maths/glm.h"
+//#include <SFML/Graphics.hpp>
 
 /* Vestigial class that was going to be used for player movement. */
 
 // WIP
-class Controller {
-    glm::vec3 translateInput();
-    sf::Vector2i mouseInput();
-};
+//class Controller {
+//    glm::vec3 translateInput();
+//    sf::Vector2i mouseInput();
+//};
 
-#endif // CONTROLLER_H_INCLUDED
+//#endif // CONTROLLER_H_INCLUDED
