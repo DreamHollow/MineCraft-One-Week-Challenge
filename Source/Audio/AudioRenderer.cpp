85d8f92aacaace4890211a704e83d1078fa7d271
DreamHollow <404errorman@gmail.com>
2024-01-01 13:53:40 -0500

    AudioRenderer: Added

---
+++ Source/Audio/AudioRenderer.cpp
@@ -0,0 +1,1 @@
+#include "Audio/AudioRenderer.h"
---
+++ include/Audio/AudioRenderer.h
@@ -0,0 +1,16 @@
+#ifndef AUDIO_RENDERER_H_INCLUDED
+#define AUDIO_RENDERER_H_INCLUDED 1
+
+#include <SFML/Audio.hpp>
+#include <vector>
+
+//struct RenderInfo;
+
+//class AudioRenderer
+//{
+//public:
+    //void add();
+    //void render();
+//};
+
+#endif
