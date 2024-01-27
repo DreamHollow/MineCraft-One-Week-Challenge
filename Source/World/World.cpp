9977888a0246dc3a5cd2c2ab55159d44b1201e75
DreamHollow <404errorman@gmail.com>
2024-01-26 21:08:29 -0500

    World.cpp: Alter thread behavior

--- Source/World/World.cpp
+++ Source/World/World.cpp
@@ -18,7 +18,7 @@ World::World(const Camera &camera, const Config &config, Player &player)
     player.position = m_playerSpawnPoint;
 
     for (int i = 0; i < 1; i++) {
-        std::this_thread::sleep_for(std::chrono::milliseconds(200));
+        std::this_thread::sleep_for(std::chrono::milliseconds(2000)); // 200 normally
         m_chunkLoadThreads.emplace_back([&]() { loadChunks(camera); });
     }
 }
