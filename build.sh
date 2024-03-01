fefa586eba88841024bd25234d8a0360e195124a
DreamHollow <404errorman@gmail.com>
2024-03-01 11:58:03 -0500

    build.sh: Modify script

--- build.sh
+++ build.sh
@@ -1,12 +1,25 @@
 #!/bin/sh
 
+BUILD_DIR='build'
+BINARY='mc-one-week'
+
 cmake -S . -B build
-if [ -d 'build' ]
+
+if [ -d $BUILD_DIR ]
 then
-	cd build
+	cd $BUILD_DIR
 	make
-	return 0
+	
+	if [ -f $BINARY ]
+	then
+		echo "Binary built successfully."
+	else
+		echo "ERROR: Binary failed to build or is corrupt!"
+		return 1
+	fi
 else
 	echo "Build directory not found!"
 	return 1
 fi
+
+return 0
