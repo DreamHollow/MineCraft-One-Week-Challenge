088db5b005bfbf1b96d6118d68baa8dd588df242
DreamHollow <404errorman@gmail.com>
2024-03-19 23:33:32 -0400

    Track.h: Commit new files

---
+++ include/Audio/Track.h
@@ -0,0 +1,18 @@
+#ifndef TRACK_HPP_INCLUDED
+#define TRACK_HPP_INCLUDED 1
+
+#include <SFML/Audio.hpp>
+#include <iostream>
+#include <vector>
+#include <memory>
+#include <fstream>
+#include <filesystem>
+
+// This is just a placeholder for now.
+
+class Track
+{
+    
+};
+
+#endif
