e71f2987ab5cfe116d572c0a66fd348bf8151828
DreamHollow <404errorman@gmail.com>
2024-03-19 23:33:58 -0400

    Globals.h: Commit new files

--- include/Debugger/Globals.h
+++ include/Debugger/Globals.h
@@ -1,6 +1,9 @@
 #ifndef GLOBALS_H_INCLUDED
 #define GLOBALS_H_INCLUDED 1
 
+// Static CONST because unless you really need to change a static variable,
+// it should remain in the same state for execution.
+
 static const bool debug = true;
 static const char db_string[8] = "DEBUG: ";
 
