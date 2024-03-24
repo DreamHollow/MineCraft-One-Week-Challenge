d89364be894ab7a5d38f75104447bfa0d07ea975
DreamHollow <404errorman@gmail.com>
2024-03-24 00:03:55 -0400

    GLFunctions.cpp: Add comment

--- Source/GL/GLFunctions.cpp
+++ Source/GL/GLFunctions.cpp
@@ -1,10 +1,14 @@
 #include "GL/GLFunctions.h"
 
+/// @brief Universally applicable triangle drawing call for OpenGL.
+/// @param indicesCount 
 void GL::drawElements(GLuint indicesCount) noexcept
 {
     glDrawElements(GL_TRIANGLES, indicesCount, GL_UNSIGNED_INT, nullptr);
 }
 
+/// @brief GL call to bind the VAO, which permits object persistence in GPU.
+/// @param vao 
 void GL::bindVAO(GLuint vao) noexcept
 {
     glBindVertexArray(vao);
