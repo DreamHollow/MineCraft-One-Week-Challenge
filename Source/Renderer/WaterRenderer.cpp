10996320bd3867309c3955ba1db6542ddb0e1d9d
DreamHollow <404errorman@gmail.com>
2024-10-12 05:32:30 -0400

    WaterRenderer.cpp: Update camera calls

--- Source/Renderer/WaterRenderer.cpp
+++ Source/Renderer/WaterRenderer.cpp
@@ -22,7 +22,7 @@ void WaterRenderer::render(const Camera &camera)
     glDisable(GL_CULL_FACE);
     m_shader.useProgram();
 
-    m_shader.loadProjectionViewMatrix(camera.getProjectionViewMatrix());
+    m_shader.loadProjectionViewMatrix(camera.m_projViewMatrix);
     m_shader.loadTime(g_timeElapsed);
 
     for (auto mesh : m_chunks) {
